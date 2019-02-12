39291e4e0af6d18457834e9ae65b69dd13891ce8
JuanNicolasGomez <nicolas.gm243@gmail.com>
2019-02-12 09:49:45 -0500

    Procfile added

---
+++ src/main/java/webApp.java
@@ -0,0 +1,20 @@
+import org.apache.log4j.BasicConfigurator;
+
+import static spark.Spark.*;
+
+public class webApp {
+
+    public static void main(String[] args) {
+        BasicConfigurator.configure();
+        port(getPort());
+        get("/hello", (req, res) -> "Hello Heroku");
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; //returns default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+}
